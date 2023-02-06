5ad8a4763b90b2bc490f496ae5b2a0d4f31e6fcf
JaemoKoo <devgoozz@gmail.com>
2023-02-06 15:34:01 +0900

    feat: 홈 화면 와이어프레임 작업

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { NavLinks } from "./models";
+import { Home } from "./pages";
+
 export default function AppRouter() {
+  const PAGE_LIST = [{ path: NavLinks.Home, element: <Home /> }];
+
   return (
-    <div>
-      <div>serverside rendering test</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
