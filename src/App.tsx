2d2a2eaa35732729ce5140d15cb3363933dabfc9
JaemoKoo <devgoozz@gmail.com>
2023-02-06 13:51:17 +0900

    chore: firebase 프로젝트 연동 및 초기화 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import AppRouter from "./AppRouter";
+import { firebaseApp } from "./services";
 import AppStyle from "./styles/AppStyle";
 
 function App() {
+
+  console.log(firebaseApp);
   return (
     <div className="App">
       <AppStyle />
